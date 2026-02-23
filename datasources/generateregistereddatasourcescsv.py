3a553201559dfce75b129dd7330117c31a931e47
UmaMahesh <167719141+UmaMahesh-Devarajula@users.noreply.github.com>
2026-02-23 23:41:42 +0530

    update

--- datasources/generateregistereddatasourcescsv.py
+++ datasources/generateregistereddatasourcescsv.py
@@ -3,9 +3,9 @@ import csv
 import datetime
 import json
 from typing import Dict, List
-from data-sources import SOURCE_TYPES
-from data-sources import PARSED_FIELDS
-from data-sources import COMMON_PROPERTIES
+from datasources.datasources import SOURCE_TYPES
+from datasources.datasources import PARSED_FIELDS
+from datasources.datasources import COMMON_PROPERTIES
 
 def get_superset_fields() -> List[str]:
     """
