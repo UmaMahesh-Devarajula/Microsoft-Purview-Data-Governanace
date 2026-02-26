a4f609781044bc1403d98c5cbf8b0bde2c6cd0a3
UmaMahesh <167719141+UmaMahesh-Devarajula@users.noreply.github.com>
2026-02-26 18:48:04 +0530

    commit

---
+++ Collections/listCollections.py
@@ -0,0 +1,12 @@
+from PurviewClient.purviewclient import get_purview_admin_client
+
+def listCollections:
+    admin_client = get_purview_admin_client()
+    try:
+        collection_list = admin_client.collections.list_collections()
+        for collection in collection_list:
+            print(collection)
+    except Exception as e:
+        # If admin call fails, fall back to user-provided name
+        print(e)
+        pass
