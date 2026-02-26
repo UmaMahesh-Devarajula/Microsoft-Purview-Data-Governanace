436caa4e94a187001973f60a48ceff60ffde4af0
UmaMahesh <167719141+UmaMahesh-Devarajula@users.noreply.github.com>
2026-02-26 23:56:05 +0530

    ert

--- Collections/restoreCollections.py
+++ Collections/restoreCollections.py
@@ -33,7 +33,7 @@ def recreate_from_csv():
                     token = cred.get_token("https://purview.azure.net/.default")
 
                     # 3. Prepare REST Request
-                    url = f" https://{r["tenant_id"]}-api.purview-service.microsoft.com/account/domains/{domain_friendly_name}?api-version=2023-12-01-preview"
+                    url = f" https://{r["tenant_id"]}-api.purview-service.microsoft.com/account/domains/{domain_name}?api-version=2023-12-01-preview"
                     headers = {
                         "Authorization": f"Bearer {token.token}",
                         "Content-Type": "application/json"
@@ -41,7 +41,7 @@ def recreate_from_csv():
                     body = {
                         "properties": {
                             "containerType":"Domain",
-                            "friendlyName": row['friendlyName'],
+                            "friendlyName": domain_friendly_name,
                             "description": row['description']
                         }
                     }
