b90ca37b2c8f29d8e21b50746c497aefcf2c1cef
UmaMahesh <167719141+UmaMahesh-Devarajula@users.noreply.github.com>
2026-03-14 14:22:17 +0530

    update

--- datasources/deletedatasource.py
+++ datasources/deletedatasource.py
@@ -1,4 +1,4 @@
-from PurviewSacnClient.purviewscanclient import get_purview_scan_client
+from PurviewScanClient.purviewscanclient import get_purview_scan_client
 
 def deletedatasource():
     ds_name = input("Enter Data source you want to delete: ")
